d55af8218a4c869400c030e2403362981b3fa6ee
Gary Klindt <gary@googlemail.com>
2014-01-17 00:18:22 +0100

    exclusion list for tags and status and weighting (layout)

--- main.py
+++ main.py
@@ -24,27 +24,208 @@ projectsASnodes = True
 
 ## fine tune node existence and connection creation
 excludedTags = ['program'] # those nodes are supressed
-excludedTaggedTaskStatus = ['completed', 'deleted'] # connection between tags and those are supressed
+excludedTaggedTaskStatus = set(['completed', 'deleted']) # connection between tags and those are supressed
+excludedTaskStatus = ['deleted'] # nodes removed
+for e in excludedTaskStatus:  # excluded tasks do not need connections
+    excludedTaggedTaskStatus.add(e)
 
-# Wrap label text at this number of characters
-charsPerLine = 20;
-# The width of the border around the tasks:
-penWidth = 1
+## edges weight
+class Weight(object):
+    def __init__(self):
+        self.task2task = 3
+        self.task2tag  = 1
+        self.task2project = 6
+weight = Weight()
+
+## layout
+layout = "fdp"
+class Misc(object):
+    def __init__(self):
+        self.charsPerLine = 20;
+        self.penWidth = 1
+misc = Misc()
 
 # colors
-blockedColor = 'gold4'
-unblockedColor = 'green'
-doneColor = 'grey'
-waitColor = 'white'
-deletedColor = 'pink'
-tagColor = 'white'
+class Colors(object):
+    def __init__(self):
+        self.blockedColor = 'gold4'
+        self.unblockedColor = 'green'
+        self.doneColor = 'grey'
+        self.waitColor = 'white'
+        self.deletedColor = 'pink'
+        self.tagColor = 'white'
+        self.elseColor = 'white'
+colors = Colors()
 
 ## graphviz
 # Left to right layout, my favorite, ganntt-ish
 # HEADER = "digraph  dependencies { splines=true; overlap=ortho; rankdir=LR; weight=2;"
 # Spread tasks on page
-HEADER = "digraph  dependencies { layout=sfdp;   splines=true; overlap=scalexy;  rankdir=LR; weight=2;"
-FOOTER = "}"
+class Dotter(object):
+
+    def __init__(self,
+                 layout,
+                 colors,
+                 weight,
+                 misc):
+        self.HEADER = "digraph  dependencies {"
+        self.HEADER += "layout={0}; ".format(layout)
+        self.HEADER += "splines=true; "
+        self.HEADER += "overlap=scalexy; "
+        self.HEADER += "rankdir=LR;"
+        self.HEADER += "weight=2;"
+        self.FOOTER = "}"
+        self.layout = layout
+        self.misc = misc
+        self.colors = colors
+        self.weight = weight
+
+
+    def project(self, project):
+        line = "\"{0}\"".format(project)
+        line += "[shape=diamond]"
+        line += "[label=\"{0}\"]".format(project)
+        line += "[color=blue]"
+        line += "[style=bold]"
+        line += "[fontcolor=red]"
+        line += "[fontsize=12]"
+        return line
+
+
+    def tag(self, tag):
+        line = "\"{0}\"".format(tag)
+        line += "[shape=ellipse]"
+        line += "[label=\"{0}\"]".format(tag)
+        line += "[fillcolor=black]"
+        line += "[style=filled]"
+        line += "[fontcolor=white]"
+        return line
+
+
+    def list(self, ll, f):
+        " convert a list to dot-file lines using the function f "
+        lines = []
+        for l in ll:
+            lines.append(f(l))
+        return lines
+
+
+    def tags(self, tags):
+        return self.list(tags, self.tag)
+
+
+    def projects(self, projects):
+        return self.list(projects, self.project)
+
+
+    def taskVStask(self, task):
+        res = []
+        if task['description']:
+            for dep in task.get('depends', '').split(','):
+                if dep != '' and dep in uuids:
+                    res.append('"%s" -> "%s"[style=bold];' % (dep, task['uuid']))
+        return res
+
+
+    def taskVStag(self, task):
+        res = []
+        if task['description']:
+            if 'tags' in task.keys():
+                for tag in task['tags']:
+                    if tag in tags and not task['status'] in excludedTaggedTaskStatus:
+                        line = "\"{0}\" -> \"{1}\"[style=dashed];".format(tag, task['uuid'])
+                        res.append(line)
+        return res
+
+
+    def taskVSproject(self,task):
+        res = []
+        if task['description']:
+            if 'project' in task.keys():
+                if not task['status'] in excludedTaskStatus:
+                    project = task['project']
+                    line = "\"{0}\" -> \"{1}\"[style=bold][color=blue];".format(project, task['uuid'])
+                    res.append(line)
+        return res
+
+
+    def task(self,task):
+
+        style = ''
+        color = ''
+        style = 'filled'
+
+        if task['status'] in excludedTaskStatus:
+            return ''
+
+        if task['status'] == 'pending':
+            prefix = ''
+            if not task.get('depends',''):
+                color = self.colors.unblockedColor
+            else :
+                hasPendingDeps = 0
+                for depend in task['depends'].split(','):
+                    for task in data:
+                        if task['uuid'] == depend and task['status'] == 'pending':
+                            hasPendingDeps = 1
+                if hasPendingDeps == 1 : color = self.colors.blockedColor
+                else : color = self.colors.unblockedColor
+
+        elif task['status'] == 'waiting':
+            prefix = 'WAIT'
+            color = self.colors.waitColor
+        elif task['status'] == 'completed':
+            prefix = 'DONE'
+            color = self.colors.doneColor
+        elif task['status'] == 'deleted':
+            prefix = 'DELETED'
+            color = self.colors.deletedColor
+        else:
+            prefix = ''
+            color = self.colors.elseColor;
+
+        label = '';
+        descriptionLines = textwrap.wrap(task['description'],
+                                         self.misc.charsPerLine);
+        for descLine in descriptionLines:
+            label += descLine + "\\n";
+
+        line = '"{0}"'.format(task['uuid'])
+        line += '[shape=note]'
+        line += '[penwidth={0}]'.format(self.misc.penWidth)
+        line += '[label="{0}: {1}"]'.format(prefix, label)
+        line += '[fillcolor={0}]'.format(color)
+        line += '[style={0}]'.format(style)
+
+        return line
+
+
+    def inputString(self):
+        res = [self.HEADER]
+
+        # nodes
+        for datum in data:
+            res.append(self.task(datum))
+        if projectsASnodes:
+            res = res + self.projects(projects)
+        if tagsASnodes:
+            res = res + self.tags(tags)
+
+        # edges
+        for datum in data:
+            res = res + self.taskVStask(datum)
+        if tagsASnodes:
+            for datum in data:
+                res = res + self.taskVStag(datum)
+        if projectsASnodes:
+            for datum in data:
+                res = res + self.taskVSproject(datum)
+
+        res.append(self.FOOTER)
+
+        return "\n".join(res)
+
+
 
 def call_taskwarrior(cmd):
     'call taskwarrior, returning output and error'
@@ -57,13 +238,13 @@ def get_json(query):
     JSON_START = '['
     JSON_END = ']'
     result, err = call_taskwarrior('export %s' % query)
-    return json.loads(JSON_START + result + JSON_END)
+    return json.loads(JSON_START + result.decode('utf8') + JSON_END)
 
 
 def call_dot(instruction):
     'call dot, returning stdout and stdout'
     dot = Popen('dot -T svg'.split(), stdout=PIPE, stderr=PIPE, stdin=PIPE)
-    return dot.communicate(instruction)
+    return dot.communicate(instruction.encode())
 
 
 def projectsFromTasks(tasks):
@@ -96,120 +277,6 @@ def uuidsFromData(data):
     return res
 
 
-def prepareTask(task):
-
-    style = ''
-    color = ''
-    style = 'filled'
-
-    if task['status']=='pending':
-        prefix = ''
-        if not task.get('depends',''):
-            color = unblockedColor
-        else :
-            hasPendingDeps = 0
-            for depend in task['depends'].split(','):
-                for task in data:
-                    if task['uuid'] == depend and task['status'] == 'pending':
-                        hasPendingDeps = 1
-            if hasPendingDeps == 1 : color = blockedColor
-            else : color = unblockedColor
-
-    elif task['status'] == 'waiting':
-        prefix = 'WAIT'
-        color = waitColor
-    elif task['status'] == 'completed':
-        prefix = 'DONE'
-        color = doneColor
-    elif task['status'] == 'deleted':
-        prefix = 'DELETED'
-        color = deletedColor
-    else:
-        prefix = ''
-        color = 'white'
-
-    label = '';
-    descriptionLines = textwrap.wrap(task['description'],charsPerLine);
-    for descLine in descriptionLines:
-        label += descLine+"\\n";
-
-    line = '"{0}"'.format(task['uuid'])
-    line += '[shape=note]'
-    line += '[penwidth={0}]'.format(penWidth)
-    line += '[label="{0}: {1}"]'.format(prefix, label)
-    line += '[fillcolor={0}]'.format(color)
-    line += '[style={0}]'.format(style)
-
-    return line
-
-
-def project2dot(project):
-    line = "\"{0}\"".format(project)
-    line += "[shape=diamond]"
-    line += "[label=\"{0}\"]".format(project)
-    line += "[color=blue]"
-    line += "[style=bold]"
-    line += "[fontcolor=blue]"
-    line += "[fontsize=10]"
-    return line
-
-
-def tag2dot(tag):
-    line = "\"{0}\"".format(tag)
-    line += "[shape=ellipse]"
-    line += "[label=\"{0}\"]".format(tag)
-    line += "[fillcolor=black]"
-    line += "[style=filled]"
-    line += "[fontcolor=white]"
-    return line
-
-
-def list2dot(ll, f):
-    " convert a list to dot-file lines using the function f "
-    lines = []
-    for l in ll:
-        lines.append(f(l))
-    return lines
-
-
-def tags2dot(tags):
-    return list2dot(tags, tag2dot)
-
-
-def projects2dot(projects):
-    return list2dot(projects, project2dot)
-
-
-def taskVStask2dot(task):
-    res = []
-    if task['description']:
-        for dep in task.get('depends', '').split(','):
-            if dep != '' and dep in uuids:
-                res.append('"%s" -> "%s"[style=bold];' % (dep, task['uuid']))
-    return res
-
-
-def taskVStag2dot(task):
-    res = []
-    if task['description']:
-        if 'tags' in task.keys():
-            for tag in task['tags']:
-                if tag in tags and task['status'] not in excludedTaggedTaskStatus:
-                    line = "\"{0}\" -> \"{1}\"[style=dashed];".format(tag, task['uuid'])
-                    res.append(line)
-    return res
-
-
-def taskVSproject2dot(task):
-    res = []
-    if task['description']:
-        if 'project' in task.keys():
-            project = task['project']
-            line = "\"{0}\" -> \"{1}\"[style=bold][color=blue];".format(project, task['uuid'])
-            res.append(line)
-    return res
-
-
 def excludeElementsFrom(toExclude, ll):
     """
     toExclude and ll are lists of elements.
@@ -223,48 +290,28 @@ def excludeElementsFrom(toExclude, ll):
     return res
 
 
+
 query = sys.argv[1:]
 data = get_json(' '.join(query))
-print(call_taskwarrior(' '.join(query))[0])
 
 # data has to be queried somehow
-
 projects = projectsFromTasks(data)
 tags = excludeElementsFrom(excludedTags, tagsFromData(data))
 uuids = uuidsFromData(data)
 
-lines = [HEADER]
+# dotfile
+dotter = Dotter(layout, colors, weight, misc)
+dotSource = dotter.inputString()
+#:print(dotSource)
 
-# nodes
-for datum in data:
-    lines.append(prepareTask(datum))
-if projectsASnodes:
-    lines = lines + projects2dot(projects)
-if tagsASnodes:
-    lines = lines + tags2dot(tags)
-
-# edges
-for datum in data:
-    lines = lines + taskVStask2dot(datum)
-
-if tagsASnodes:
-    for datum in data:
-        lines = lines + taskVStag2dot(datum)
-
-if projectsASnodes:
-    for datum in data:
-        lines = lines + taskVSproject2dot(datum)
-
-
-lines.append(FOOTER)
-
-png, err = call_dot('\n'.join(lines))
-if err != '':
+## calling dot
+png, err = call_dot(dotSource)
+if err != b'':
     print ('Error calling dot:')
     print (err.strip())
 
 print ('Writing to deps.svg')
 with open('deps.svg', 'w') as f:
-    f.write(png)
+    f.write(png.decode('utf-8'))
 
 
